9b1575fba8704779444f3bbc4bfd223133cd1d43
Jaekyun Seok <jaekyun@google.com>
2017-04-05 11:23:26 +0900

    sm6250-common: Enforce RRO on framework-res
    Overlays only for framework-res will be converted into RROs.
    
    Other overlays can't be converted due to some known issues on app RRO.
    
    Bug: 36231603
    Change-Id: Idca30fdbbcc990fd124de16a06a112a346612a61
    (cherry picked from commit db1b6058200fc5255f519cf6609fca7619d6743e)

--- common.mk
+++ common.mk
@@ -290,7 +290,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res
 
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
