b4759419e20810af9c95fe6a6139de8d3a8b2a6a
SonalSingh18 <sonal.singh.19993@gmail.com>
2021-02-16 17:37:01 +0530

    sm6250-common: XiaomiParts: Add Fastcharge toggle
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- XiaomiParts/src/com/xiaomi/parts/DeviceSettings.java
+++ XiaomiParts/src/com/xiaomi/parts/DeviceSettings.java
@@ -50,6 +50,10 @@ public class DeviceSettings extends PreferenceFragment implements
     public static final String PREF_USB_FASTCHARGE = "usb_charge";
     public static final String USB_FASTCHARGE_PATH = "/sys/kernel/fast_charge/force_fast_charge";
 
+    public static final String CATEGORY_FASTCHARGE = "fastcharge";
+    public static final String PREF_FASTCHARGE = "fast_charge";
+    public static final String FASTCHARGE_PATH = "/sys/class/power_supply/bms/fastcharge_mode";
+
     public static final String PREF_KEY_FPS_INFO = "fps_info";
 
     public static final String PREF_MSM_TOUCHBOOST = "touchboost";
@@ -61,6 +65,7 @@ public class DeviceSettings extends PreferenceFragment implements
     private SecureSettingListPreference mHeadsetType;
     private SecureSettingListPreference mPreset;
     private SecureSettingSwitchPreference mUSBFastcharge;
+    private SecureSettingSwitchPreference mFastcharge;
     private SecureSettingSwitchPreference mTouchboost;
     private static Context mContext;
 
@@ -133,6 +138,16 @@ public class DeviceSettings extends PreferenceFragment implements
         } else {
             getPreferenceScreen().removePreference(findPreference(CATEGORY_USB_FASTCHARGE));
         }
+
+        if (FileUtils.fileWritable(FASTCHARGE_PATH)) {
+            mFastcharge = (SecureSettingSwitchPreference) findPreference(PREF_FASTCHARGE);
+            mFastcharge.setEnabled(Fastcharge.isSupported());
+            mFastcharge.setChecked(Fastcharge.isCurrentlyEnabled(this.getContext()));
+            mFastcharge.setOnPreferenceChangeListener(new Fastcharge(getContext()));
+        }
+//          else {
+//            getPreferenceScreen().removePreference(findPreference(CATEGORY_FASTCHARGE));
+//        }
 
         SwitchPreference fpsInfo = (SwitchPreference) findPreference(PREF_KEY_FPS_INFO);
         fpsInfo.setChecked(prefs.getBoolean(PREF_KEY_FPS_INFO, false));
