47ed1a0ad7b1468b9b50397280c20885c169efea
Alexander Winkowski <dereference23@outlook.com>
2021-08-05 14:48:13 +0200

    sm6250-common: Import Pixel thermal HAL
    * From hardware/google/pixel at 728fb99bbb910be05711421310efa6827aaaa4fa.
    
    Change-Id: I763b4dbef65084cfee337065b2c5ab465f69bca8

--- common.mk
+++ common.mk
@@ -404,6 +404,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
 
+# Thermal HAL
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.pixel
+
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
